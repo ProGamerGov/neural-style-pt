8ad97640f9e7bf6f3d642df3ec4a217b26deda93
ProGamerGov <ProGamerGov@users.noreply.github.com>
2019-10-05 09:57:32 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="neural-style",
-    version="0.4.3",
+    version="0.4.4",
     author="ProGamerGov",
     description="A PyTorch implementation of artistic style transfer",
     long_description=long_description,
