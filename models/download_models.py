04eab2aafd84a47bff99b2021d66f50589a147b4
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-02-15 11:27:38 -0700

    Update download_models.py

--- models/download_models.py
+++ models/download_models.py
@@ -7,14 +7,14 @@ from torch.utils.model_zoo import load_url
 
 # Download the VGG-19 model and fix the layer names
 print("Downloading the VGG-19 model")
-sd = load_url("https://s3-us-west-2.amazonaws.com/jcjohns-models/vgg19-d01eb7cb.pth")
+sd = load_url("https://web.eecs.umich.edu/~justincj/models/vgg19-d01eb7cb.pth")
 map = {'classifier.1.weight':u'classifier.0.weight', 'classifier.1.bias':u'classifier.0.bias', 'classifier.4.weight':u'classifier.3.weight', 'classifier.4.bias':u'classifier.3.bias'}
 sd = OrderedDict([(map[k] if k in map else k,v) for k,v in sd.items()])
 torch.save(sd, path.join("models", "vgg19-d01eb7cb.pth"))
 
 # Download the VGG-16 model and fix the layer names
 print("Downloading the VGG-16 model")
-sd = load_url("https://s3-us-west-2.amazonaws.com/jcjohns-models/vgg16-00b39a1b.pth")
+sd = load_url("https://web.eecs.umich.edu/~justincj/models/vgg16-00b39a1b.pth")
 map = {'classifier.1.weight':u'classifier.0.weight', 'classifier.1.bias':u'classifier.0.bias', 'classifier.4.weight':u'classifier.3.weight', 'classifier.4.bias':u'classifier.3.bias'}
 sd = OrderedDict([(map[k] if k in map else k,v) for k,v in sd.items()])
 torch.save(sd, path.join("models", "vgg16-00b39a1b.pth"))
